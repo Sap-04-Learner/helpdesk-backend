28896672d9ecd3e80f401ace85f993e1cf64b486
Jagdish Everest <jagdisheverest23@gmail.com>
2026-02-23 21:20:10 +0530

    feat(assets): implement core asset management module and prisma schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TicketsModule } from './modules/tickets/tickets.module';
+import { AssetsModule } from './assets/assets.module';
 
 @Module({
-  imports: [TicketsModule],
+  imports: [TicketsModule, AssetsModule],
   controllers: [AppController],
   providers: [AppService],
 })
