c6e60f5eb90c993d08e68b8344aa3fe38e1a78dd
Saumyadeep Pal <saumyadeep2004@gmail.com>
2026-03-01 16:10:58 +0530

    feat(it): create users service in users module
    - changed prisma schema to use nanoid as user and ticket ids
    - create users dir using  'nest g res modules/users'
    - create methods in users services file
    - apply argon2 to hash the password

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { TicketsModule } from './modules/tickets/tickets.module';
 import { AssetsModule } from './modules/assets/assets.module';
 import { PrismaService } from './prisma.service';
 import { MockAuthModule } from './modules/mock-auth/mock-auth.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [TicketsModule, AssetsModule, MockAuthModule],
+  imports: [TicketsModule, AssetsModule, MockAuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
